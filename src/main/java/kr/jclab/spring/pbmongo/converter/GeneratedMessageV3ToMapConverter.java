9a373fb46717cd64c8597c99b43baf9706c7191e
jichan <development@jc-lab.net>
2021-03-12 16:27:49 +0900

    feat: support map type

--- src/main/java/kr/jclab/spring/pbmongo/converter/GeneratedMessageV3ToMapConverter.java
+++ src/main/java/kr/jclab/spring/pbmongo/converter/GeneratedMessageV3ToMapConverter.java
@@ -2,10 +2,12 @@ package kr.jclab.spring.pbmongo.converter;
 
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.GeneratedMessageV3;
+import com.google.protobuf.MapEntry;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GeneratedMessageV3ToMapConverter implements Converter<GeneratedMessageV3, Map<String, Object>>  {
@@ -15,12 +17,25 @@ public class GeneratedMessageV3ToMapConverter implements Converter<GeneratedMess
         this.mongoConverter = mongoConverter;
     }
 
+    private Map convertMap(List list) {
+        Map map = new HashMap();
+        for (Object item : list) {
+            MapEntry entry = (MapEntry)item;
+            map.put(entry.getKey(), mongoConverter.convertToMongoType(entry.getValue()));
+        }
+        return map;
+    }
+
     @Override
     public Map<String, Object> convert(GeneratedMessageV3 source) {
         HashMap<String, Object> map = new HashMap<>();
         for (Descriptors.FieldDescriptor entry : source.getDescriptorForType().getFields()) {
             Object value = source.getField(entry);
-            map.put(entry.getName(), mongoConverter.convertToMongoType(value));
+            if (entry.isMapField()) {
+                map.put(entry.getName(), convertMap((List)value));
+            } else {
+                map.put(entry.getName(), mongoConverter.convertToMongoType(value));
+            }
         }
         return map;
     }
